adf14d016323cea1d74d7fb67d7c4e68da37f174
Nicole Bernardo <nicolebernardo96@gmail.com>
2022-01-05 17:10:23 -0800

    Added Intro Section To Portfolio (#1)
    * added image and text to intro section, added styling and media queries for responsiveness
    
    * removed commented out code

--- components/intro.js
+++ components/intro.js
@@ -1,11 +1,30 @@
 import Image from 'next/image'
+import { name } from '../components/layout'
 import styles from './styles/intro.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 
 export default function Intro() {
     return (
         <div className={styles.container}>
-            
+            <div className={styles.imageContainer}>
+                <Image
+                src="/images/profile.jpg"
+                className={`${utilStyles.borderCircle} ${styles.portfolioImage} img-fluid`}
+                height={400}
+                width={400}
+                alt={name}
+                priority
+                responsive
+                />
+            </div>
+            <div className={styles.textContainer}>
+                <p className={styles.introText}>
+                Hi, my name is Nicole Bernardo. I’m a Software Engineer with a passion 
+                for building and maintaining sites both frontend and backend, and a knack 
+                for creating things outside the world of software engineering.
+                </p>
+            </div>
         </div>
     )
 }
