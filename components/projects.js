b2414d4e3d1f851c8b8296bda4a6be35e3c93ca4
Nicole <nicolebernardo96@gmail.com>
2022-01-09 00:58:26 -0800

    adjusted project container padding, aremoved commas and added spaces between technologies

--- components/projects.js
+++ components/projects.js
@@ -22,12 +22,12 @@ export default function Projects() {
                     />
                 </div>
                 <h1 className={styles.projectTitle}>Sunpunch</h1>
-                <p className={styles.projectTechnologies}>Technologies Used: React, NodeJS, Express, KnexJS</p>
+                <p className={styles.projectTechnologies}>React &nbsp; NodeJS &nbsp; Express &nbsp; KnexJS</p>
                 <i className={`${styles.projectLinks} fab fa-github`}></i>
                 <div className={styles.projectDetailsContainer}>
                     <h1>Sunpunch</h1>
                     <p className={styles.projectDetails}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p className={styles.projectTechnologiesInner}>Technologies Used: React, NodeJS, Express, KnexJS</p>
+                    <p className={styles.projectTechnologiesInner}>React &nbsp; NodeJS &nbsp; Express &nbsp; KnexJS</p>
                     <i className={`${styles.projectLinksInner} fab fa-github`}></i>
                 </div>
             </div>
@@ -45,12 +45,12 @@ export default function Projects() {
                     />
                 </div>
                 <h1 className={`${styles.projectTitle} ${utilStyles.mirrored}`}>Sunpunch</h1>
-                <p className={`${styles.projectTechnologiesFlipped} ${utilStyles.mirrored}`}>Technologies Used: React, NodeJS, Express, KnexJS</p>
+                <p className={`${styles.projectTechnologiesFlipped} ${utilStyles.mirrored}`}>React &nbsp; NodeJS &nbsp; Express &nbsp; KnexJS</p>
                 <i className={`${styles.projectLinksFlipped} ${utilStyles.mirrored} fab fa-github`}></i>
                 <div className={`${styles.projectDetailsContainer} ${utilStyles.mirrored} ${utilStyles.alignTextStart}`}>
                     <h1>Sunpunch</h1>
                     <p className={`${styles.projectDetails}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p className={styles.projectTechnologiesInner}>Technologies Used: React, NodeJS, Express, KnexJS</p>
+                    <p className={styles.projectTechnologiesInner}>React &nbsp; NodeJS &nbsp; Express &nbsp; KnexJS</p>
                     <i className={`${styles.projectLinksInner} fab fa-github`}></i>
                 </div>
             </div>
